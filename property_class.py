4b60d9222f3f595bffb436a7f9b61b3e02f54039
kumarg160491 <kumarg160491@gmail.com>
2022-07-03 23:21:31 +0530

    updated some changes

--- property_class.py
+++ property_class.py
@@ -1,18 +1,20 @@
+# Encapsulation concept with property decorator
+
 class Student:
 
     def __init__(self, marks):
-        self.marks = marks
+        self.__marks = marks
 
     def percentage(self):
-        return (self.marks / 600) * 100
+        return (self.__marks / 600) * 100
 
     @property
     def marks(self):
-        return self.marks
+        return self.__marks
 
     @marks.setter
     def marks(self, values):
-        self.marks = values
+        self.__marks = values
 
 s = Student(400)
 s.marks = 500
